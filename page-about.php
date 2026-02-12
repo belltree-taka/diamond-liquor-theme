e8b00839719b88edca91d75dee3b0eb661a01c83
Jaskeerat Singh <157443893+jaskeeratsingh218@users.noreply.github.com>
2026-02-12 13:32:56 -0700

    not main file

--- page-about.php
+++ page-about.php
@@ -1,17 +1,15 @@
 <?php
-
 /*
 Template Name: About
 */
-
 get_header();
 ?>
 
-	<main id="primary" class="site-main about">
-		<div class="container">
-			<?php the_content();?>
-		</div>
-	</main>
+<main id="primary" class="site-main about-page">
+	<div class="container">
+		<?php the_content(); ?>
+	</div>
+</main>
 
 <?php
 get_footer();
