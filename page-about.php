4bfb8ff0ca9ed0a6047915eeb773a9e391e1d4a6
Takaharu Suzuki <taka.webtree@gmail.com>
2026-03-11 21:24:15 -0600

    pushing the latest working code with brand new about page

--- page-about.php
+++ page-about.php
@@ -6,7 +6,10 @@ Template Name: About
 
 get_header();
 ?>
-
+	<section class="hero-banner hero-banner--about">
+		<?php the_post_thumbnail('full'); ?>
+		<h2 class="hero-banner__about-page-title">About Us</h2>
+	</section>
 	<main id="primary" class="site-main about">
 		<?php the_content();?>
 	</main>
